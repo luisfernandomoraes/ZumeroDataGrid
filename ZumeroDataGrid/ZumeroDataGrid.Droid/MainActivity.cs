14e7dda8a8721ce1618fe52389faed24b0eb49d7
Luís Fernando <luisfernando.pereira.moraes@gmail.com>
2016-04-06 11:02:50 -0300

    Add project files.

---
+++ ZumeroDataGrid/ZumeroDataGrid.Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ZumeroDataGrid.Droid
+{
+    [Activity(Label = "ZumeroDataGrid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Zumero.DataGridComponent.Init();
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+    }
+}
+
